4f2fdc045ed45bbc56365406705b4d9a85acefd0
AleksandarDmitrovic <aleksandar.dmitrovic@gmail.com>
2024-05-24 14:12:24 -0600

    Creeate dropdown for product quantities in cart

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -12,6 +12,15 @@ interface CartEntryProps {
 export default function CartEntry({
   cartItem: { product, quantity },
 }: CartEntryProps) {
+  const quantityOptions: JSX.Element[] = [];
+  for (let i = 1; i <= 99; i++) {
+    quantityOptions.push(
+      <option value={i} key={i}>
+        {i}
+      </option>,
+    );
+  }
+
   return (
     <div>
       <div className="flex flex-wrap items-center gap-3">
@@ -27,6 +36,15 @@ export default function CartEntry({
             {product.name}
           </Link>
           <div>Price: {formatPrice(product.price)}</div>
+          <div className="my-1 flex items-center gap-2">
+            Quantity:
+            <select
+              className="select select-bordered w-full max-w-[80px]"
+              defaultValue={quantity}
+            >
+              {quantityOptions}
+            </select>
+          </div>
           <div className="flex items-center gap-2">
             Total: {formatPrice(product.price * quantity)}
           </div>
