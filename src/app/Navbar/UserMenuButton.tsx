5322e44fee99ae034b6892dc09afffddffb459bf
AleksandarDmitrovic <aleksandar.dmitrovic@gmail.com>
2024-06-07 14:30:59 -0600

    Initiate usermenubutton component

---
+++ src/app/Navbar/UserMenuButton.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Session } from "next-auth";
+import Image from "next/image";
+import profilePicPlaceholder from "@/assets/profile-pic-placeholder.png";
+
+interface UserMenuButtonProps {
+  session: Session | null;
+}
+
+export default function UserMenuButton({ session }: UserMenuButtonProps) {
+  const user = session?.user;
+
+  return (
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-ghost btn-circle">
+        {user ? (
+          <Image
+            src={user?.image || profilePicPlaceholder}
+            alt="Profile Picture"
+            width={40}
+            height={40}
+            className="w-10 rounded-full"
+          />
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current"
+          ></svg>
+        )}
+      </label>
+    </div>
+  );
+}
