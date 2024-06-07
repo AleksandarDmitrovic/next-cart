f5227c4a4e7a30ab8bacbbfa82a1476e751014b4
AleksandarDmitrovic <aleksandar.dmitrovic@gmail.com>
2024-06-07 14:41:03 -0600

    Add user menu dropdown to navbar

--- src/app/Navbar/UserMenuButton.tsx
+++ src/app/Navbar/UserMenuButton.tsx
@@ -29,12 +29,19 @@ export default function UserMenuButton({ session }: UserMenuButtonProps) {
             fill="none"
             viewBox="0 0 24 24"
             className="inline-block h-5 w-5 stroke-current"
-          ></svg>
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            />
+          </svg>
         )}
       </label>
       <ul
         tabIndex={0}
-        className="dropdown-content menu rounded-box menu-sm z-30 mt-3 w-52 bg-blue-100 p-2 shadow"
+        className="dropdown-content menu rounded-box menu-sm z-30 mt-3 w-52 bg-base-100 p-2 shadow"
       >
         <li>
           {user ? (
