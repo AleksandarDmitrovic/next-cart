e7e42f2590f762bb62e5d9ac093704c517ba3761
AleksandarDmitrovic <aleksandar.dmitrovic@gmail.com>
2024-06-07 16:05:09 -0600

    Adapt create cart function to use user info

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -1,6 +1,8 @@
 import { cookies } from "next/headers";
 import prisma from "./prisma";
 import { Cart, Prisma } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export type CartWithProducts = Prisma.CartGetPayload<{
   include: { items: { include: { product: true } } };
@@ -33,18 +35,27 @@ export async function getCart(): Promise<ShoppingCart | null> {
     size: cart.items.reduce((acc, item) => acc + item.quantity, 0),
     subtotal: cart.items.reduce(
       (acc, item) => acc + item.quantity * item.product.price,
-      0,
+      0
     ),
   };
 }
 
 export async function createCart(): Promise<ShoppingCart> {
-  const newCart = await prisma.cart.create({
-    data: {},
-  });
+  const session = await getServerSession(authOptions);
 
-  cookies().set("localCartId", newCart.id);
-  // Should encrypt later on
+  let newCart: Cart;
+  if (session) {
+    newCart = await prisma.cart.create({
+      data: { userId: session.user.id },
+    });
+  } else {
+    newCart = await prisma.cart.create({
+      data: {},
+    });
+
+    cookies().set("localCartId", newCart.id);
+    // Should encrypt later on
+  }
 
   return { ...newCart, items: [], size: 0, subtotal: 0 };
 }
