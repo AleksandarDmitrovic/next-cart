c3b50e1f06d197c53fe50173d8d50fb332b5511f
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2024-11-27 15:21:36 -0700

    Hide hero product after first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,30 +30,32 @@ export default async function Home({
   return (
     <div className="flex flex-col items-center">
       <div className="hero rounded-xl bg-base-200">
-        <div className="hero-content flex-col lg:flex-row">
-          <Image
-            src={products[0].imageUrl}
-            alt={products[0].name}
-            width={400}
-            height={800}
-            className="w-full max-w-sm rounded-lg shadow-2x1"
-            priority
-          />
-          <div>
-            <h1 className="text-5xl font-bold">{products[0].name}</h1>
-            <p className="py-6">{products[0].description}</p>
-            <Link
-              href={"/products/" + products[0].id}
-              className="btn btn-primary"
-            >
-              CHECK IT OUT
-            </Link>
+        {currentPage === 1 && (
+          <div className="hero-content flex-col lg:flex-row">
+            <Image
+              src={products[0].imageUrl}
+              alt={products[0].name}
+              width={400}
+              height={800}
+              className="w-full max-w-sm rounded-lg shadow-2x1"
+              priority
+            />
+            <div>
+              <h1 className="text-5xl font-bold">{products[0].name}</h1>
+              <p className="py-6">{products[0].description}</p>
+              <Link
+                href={"/products/" + products[0].id}
+                className="btn btn-primary"
+              >
+                CHECK IT OUT
+              </Link>
+            </div>
           </div>
-        </div>
+        )}
       </div>
 
       <div className="my-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-        {products.slice(1).map((product) => (
+        {(currentPage === 1 ? products.slice(1) : products).map((product) => (
           <ProductCard product={product} key={product.id} />
         ))}
       </div>
