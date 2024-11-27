77e060e356184d212f59bc7f6645c70b14037e87
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2024-11-27 15:31:10 -0700

    Create prisma call to find all searched products

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,19 @@
+import prisma from "@/lib/db/prisma";
+
 interface SearchPageProps {
   searchParams: { query: string };
 }
 
-export default function SearchPage({
+export default async function SearchPage({
   searchParams: { query },
-}: SearchPageProps) {}
+}: SearchPageProps) {
+  const products = await prisma.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: query, mode: "insensitive" } },
+        { description: { contains: query, mode: "insensitive" } },
+      ],
+    },
+    orderBy: { id: "desc" },
+  });
+}
